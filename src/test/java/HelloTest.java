9f62594936525b71c8596915ef1fb277a873dec6
Benjamin Mette <benjamin.mette@neuland-bfi.de>
2023-09-27 10:10:30 +0200

    Code Coverage via codacy

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloTest {
+    @Test
+    void name() {
+        assertThat(true).isTrue();
+    }
+}
