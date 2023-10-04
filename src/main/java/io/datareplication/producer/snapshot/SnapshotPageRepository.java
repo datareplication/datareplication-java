83467291930192b1e651c2e550e695988d22c65d
Felix Krull <felix.krull@neuland-bfi.de>
2023-10-04 17:09:07 +0200

    api: don't keep snapshot id in SnapshotPageHeader, but we do have to pass it to SnapshotPageRepository

--- src/main/java/io/datareplication/producer/snapshot/SnapshotPageRepository.java
+++ src/main/java/io/datareplication/producer/snapshot/SnapshotPageRepository.java
@@ -1,12 +1,16 @@
 package io.datareplication.producer.snapshot;
 
 import io.datareplication.model.Page;
+import io.datareplication.model.PageId;
 import io.datareplication.model.snapshot.SnapshotEntityHeader;
+import io.datareplication.model.snapshot.SnapshotId;
 import io.datareplication.model.snapshot.SnapshotPageHeader;
 import lombok.NonNull;
 
 import java.util.concurrent.CompletionStage;
 
 public interface SnapshotPageRepository {
-    @NonNull CompletionStage<Void> save(@NonNull Page<@NonNull SnapshotPageHeader, @NonNull SnapshotEntityHeader> page);
+    @NonNull CompletionStage<Void> save(@NonNull SnapshotId snapshotId,
+                                        @NonNull PageId pageId,
+                                        @NonNull Page<@NonNull SnapshotPageHeader, @NonNull SnapshotEntityHeader> page);
 }
