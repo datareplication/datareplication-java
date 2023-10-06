ab26ef39a6ce0e85b4febe8dc8b97ebc2d69b8e7
Felix Krull <felix.krull@neuland-bfi.de>
2023-10-06 16:22:02 +0200

    internal: I guess we also need parser for header values
    You know, the 'multipart/mixed; boundary=meow' type things.

---
+++ src/main/java/io/datareplication/internal/http/HeaderFieldValue.java
@@ -0,0 +1,114 @@
+package io.datareplication.internal.http;
+
+import lombok.Value;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+
+@Value
+public class HeaderFieldValue {
+    String mainValue;
+    Map<String, String> parameters;
+
+    private static class ParameterParser {
+        private final String input;
+        private int idx;
+        private final Map<String, String> parameters;
+
+        private ParameterParser(final String input, final int idx) {
+            this.input = input;
+            this.idx = idx;
+            this.parameters = new HashMap<>();
+        }
+
+        private void parseNextParameter() {
+            expect(';');
+            eatWhitespace();
+            String name = parameterName();
+            expect('=');
+            eatWhitespace();
+            String value = parameterValue();
+            parameters.put(name, value);
+        }
+
+        private String parameterName() {
+            final int start = idx;
+            advanceWhile(c -> c != '=' && c != ';');
+            return input.substring(start, idx).trim();
+        }
+
+        private String parameterValue() {
+            if (atEnd()) {
+                return "";
+            }
+            if (peek() == '"') {
+                return quotedString();
+            }
+            final int start = idx;
+            advanceWhile(c -> c != ';');
+            return input.substring(start, idx).trim();
+        }
+
+        private String quotedString() {
+            expect('"');
+            final StringBuilder value = new StringBuilder();
+            while (!atEnd() && peek() != '"') {
+                if (peek() == '\\') {
+                    idx++;
+                }
+                value.append(peek());
+                idx++;
+            }
+            expect('"');
+            eatWhitespace();
+            return value.toString();
+        }
+
+        private void advanceWhile(Predicate<Character> condition) {
+            while (!atEnd() && condition.test(peek())) {
+                idx++;
+            }
+        }
+
+        private void eatWhitespace() {
+            advanceWhile(Character::isWhitespace);
+        }
+
+        private void expect(char expected) {
+            if (atEnd()) {
+                throw new IllegalArgumentException(String.format("expected %s, found EOF", expected));
+            }
+            final char actual = peek();
+            if (actual != expected) {
+                throw new IllegalArgumentException(String.format("expected %s, found %s", expected, actual));
+            }
+            idx++;
+        }
+
+        private char peek() {
+            if (atEnd()) {
+                throw new IllegalArgumentException("expected more input, found EOF");
+            }
+            return input.charAt(idx);
+        }
+
+        private boolean atEnd() {
+            return idx >= input.length();
+        }
+    }
+
+    public static HeaderFieldValue parse(String headerField) {
+        int mainValueEnd = headerField.indexOf(";");
+        if (mainValueEnd == -1) {
+            return new HeaderFieldValue(headerField.trim(), Collections.emptyMap());
+        }
+        final String mainValue = headerField.substring(0, mainValueEnd).trim();
+        final ParameterParser parser = new ParameterParser(headerField, mainValueEnd);
+        while (!parser.atEnd()) {
+            parser.parseNextParameter();
+        }
+        return new HeaderFieldValue(mainValue, parser.parameters);
+    }
+}
