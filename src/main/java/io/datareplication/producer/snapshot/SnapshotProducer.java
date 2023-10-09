f525b47af9258c2e7d522c6ef61d9dae94bdc4af
Benjamin Mette <benjamin.mette@neuland-bfi.de>
2023-10-09 18:21:40 +0200

    Working on SnapshotProducer

--- src/main/java/io/datareplication/producer/snapshot/SnapshotProducer.java
+++ src/main/java/io/datareplication/producer/snapshot/SnapshotProducer.java
@@ -3,6 +3,8 @@ package io.datareplication.producer.snapshot;
 import io.datareplication.model.Entity;
 import io.datareplication.model.snapshot.SnapshotEntityHeader;
 import io.datareplication.model.snapshot.SnapshotIndex;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
 
 import java.util.concurrent.CompletionStage;
@@ -11,4 +13,16 @@ import java.util.concurrent.Flow;
 public interface SnapshotProducer {
     @NonNull CompletionStage<@NonNull SnapshotIndex> produce(
         @NonNull Flow.Publisher<@NonNull Entity<@NonNull SnapshotEntityHeader>> entities);
+
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    class Builder {
+
+        public @NonNull SnapshotProducer build() {
+            throw new RuntimeException("not implemented");
+        }
+    }
+
+    static @NonNull SnapshotProducer.Builder builder() {
+        return new SnapshotProducer.Builder();
+    }
 }
--- src/test/java/io/datareplication/producer/snapshot/SnapshotProducerIntegrationTest.java
+++ src/test/java/io/datareplication/producer/snapshot/SnapshotProducerIntegrationTest.java
@@ -1,14 +1,65 @@
 package io.datareplication.producer.snapshot;
 
+import io.datareplication.model.PageId;
+import io.datareplication.model.Timestamp;
+import io.datareplication.model.Url;
+import io.datareplication.model.snapshot.SnapshotId;
+import io.datareplication.model.snapshot.SnapshotIndex;
+import lombok.NonNull;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.time.Instant;
+import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class SnapshotProducerIntegrationTest {
     @Test
-    void shouldProduceSnapshot() {
-        // TODO: Create Snapshot with Snapshot Producer
+    @DisplayName("Should produce snapshot")
+    void shouldProduceSnapshot(@Mock SnapshotPageRepository snapshotPageRepository,
+                               @Mock SnapshotIndexRepository snapshotIndexRepository,
+                               @Mock SnapshotPageUrlBuilder snapshotPageUrlBuilder,
+                               @Mock PageIdProvider pageIdProvider,
+                               @Mock SnapshotIdProvider snapshotIdProvider) {
+        Timestamp now = Timestamp.of(Instant.now());
+        SnapshotId snapshotId = SnapshotId.of("snapshotIdForTest");
+        PageId pageId1 = PageId.of("1");
+        PageId pageId2 = PageId.of("2");
+        PageId pageId3 = PageId.of("3");
+        List<Url> pages = List.of(
+            url(snapshotId, pageId1),
+            url(snapshotId, pageId2),
+            url(snapshotId, pageId3)
+        );
 
-        fail("TODO: Assert Snapshot Format on Http Server");
+        when(pageIdProvider.newPageId()).thenReturn(pageId1).thenReturn(pageId2).thenReturn(pageId3);
+        when(snapshotIdProvider.newSnapshotId()).thenReturn(snapshotId);
+        when(snapshotPageUrlBuilder.pageUrl(any(SnapshotId.class), any(PageId.class))).thenAnswer(input ->
+            url(input.getArgument(0), input.getArgument(1)));
+
+        SnapshotIndex expectedSnapshotIndex = new SnapshotIndex(snapshotId, now, pages);
+
+        SnapshotProducer snapshotProducer = SnapshotProducer
+            .builder()
+            // TODO: Additional configuration
+            .build();
+
+        // TODO: Create Snapshot with Snapshot Producer
+        // TODO Use Entities instead of null
+        snapshotProducer.produce(null);
+
+        verify(snapshotIndexRepository).save(expectedSnapshotIndex);
+        // TODO: Assert PageRepository
+    }
+
+    private @NonNull Url url(final SnapshotId snapshotId, final PageId pageId1) {
+        return Url.of("https://localhost:8080/" + snapshotId.value() + "/" + pageId1.value());
     }
 }
