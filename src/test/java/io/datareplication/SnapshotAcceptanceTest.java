b5536cebce43a78c38b6c848454ae641d83e69c2
Benjamin Mette <benjamin.mette@neuland-bfi.de>
2023-10-18 09:58:51 +0200

    InMemoryRepositories

--- src/test/java/io/datareplication/SnapshotAcceptanceTest.java
+++ src/test/java/io/datareplication/SnapshotAcceptanceTest.java
@@ -4,9 +4,14 @@ import io.datareplication.consumer.snapshot.SnapshotConsumer;
 import io.datareplication.model.Body;
 import io.datareplication.model.Entity;
 import io.datareplication.model.HttpHeaders;
+import io.datareplication.model.PageId;
 import io.datareplication.model.Url;
 import io.datareplication.model.snapshot.SnapshotEntityHeader;
+import io.datareplication.model.snapshot.SnapshotId;
 import io.datareplication.model.snapshot.SnapshotIndex;
+import io.datareplication.model.snapshot.testhelper.SnapshotIndexInMemoryRepository;
+import io.datareplication.model.snapshot.testhelper.SnapshotPageInMemoryRepository;
+import io.datareplication.producer.snapshot.SnapshotPageUrlBuilder;
 import io.datareplication.producer.snapshot.SnapshotProducer;
 import io.reactivex.rxjava3.core.Flowable;
 import lombok.NonNull;
@@ -27,10 +32,19 @@ public class SnapshotAcceptanceTest {
             .map(this::toSnapshotEntity);
         // TODO: Create Snapshot with Snapshot Producer -> Serve via Http Server
 
+        SnapshotIndexInMemoryRepository snapshotRepository = new SnapshotIndexInMemoryRepository();
+        SnapshotPageInMemoryRepository snapshotPageRepository = new SnapshotPageInMemoryRepository();
+        SnapshotPageUrlBuilder snapshotPageUrlBuilder = new SnapshotPageUrlBuilder() {
+            @Override
+            public @NonNull Url pageUrl(@NonNull final SnapshotId snapshotId, @NonNull final PageId pageId) {
+                // TODO: Server prefix
+                return Url.of(snapshotId.value() + "/" + pageId.value());
+            }
+        };
         SnapshotProducer snapshotProducer = SnapshotProducer
             .builder()
             // TODO: Additional configuration & use InMemoryRepositories
-            .build(null, null, null);
+            .build(snapshotRepository, snapshotPageRepository, snapshotPageUrlBuilder);
         SnapshotIndex producedSnapshotIndex = snapshotProducer
             .produce(FlowAdapters.toFlowPublisher(entityFlow))
             .toCompletableFuture()
---
+++ src/test/java/io/datareplication/model/snapshot/testhelper/SnapshotIndexInMemoryRepository.java
@@ -0,0 +1,31 @@
+package io.datareplication.model.snapshot.testhelper;
+
+import io.datareplication.model.snapshot.SnapshotId;
+import io.datareplication.model.snapshot.SnapshotIndex;
+import io.datareplication.producer.snapshot.SnapshotIndexRepository;
+import lombok.NonNull;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SnapshotIndexInMemoryRepository implements SnapshotIndexRepository {
+    private final ConcurrentHashMap<SnapshotId, SnapshotIndex> repository = new ConcurrentHashMap<>();
+
+    @Override
+    public @NonNull CompletionStage<Void> save(@NonNull final SnapshotIndex snapshotIndex) {
+        return CompletableFuture.supplyAsync(() -> {
+            repository.put(snapshotIndex.id(), snapshotIndex);
+            return null;
+        });
+    }
+
+    public Optional<SnapshotIndex> findBy(SnapshotId id) {
+        if (repository.contains(id)) {
+            return Optional.of(repository.get(id));
+        } else {
+            return Optional.empty();
+        }
+    }
+}
