10fd52f9a132c0ee08c304f8feb7ffcab056e683
Felix Krull <felix.krull@neuland-bfi.de>
2023-10-19 16:52:13 +0200

    page: tests for ToCompleteEntitiesTransformer

--- src/main/java/io/datareplication/internal/page/ToCompleteEntitiesTransformer.java
+++ src/main/java/io/datareplication/internal/page/ToCompleteEntitiesTransformer.java
@@ -11,17 +11,34 @@ import java.nio.channels.Channels;
 import java.nio.channels.WritableByteChannel;
 import java.util.Optional;
 
-class ToCompleteEntitiesTransformer<EntityHeader extends ToHttpHeaders> {
+/**
+ * Transform a stream of {@link StreamingPage.Chunk} objects into complete {@link Entity Entities}. This class mostly
+ * collects body bytes internally until the end of an entity is signaled and then returns the complete entity with its
+ * entire body.
+ * @param <EntityHeader> the header type for the entities; this is just passed through
+ */
+public class ToCompleteEntitiesTransformer<EntityHeader extends ToHttpHeaders> {
+    // only null on construction, gets initialized when we start our first entity
     private StreamingPage.Chunk.Header<EntityHeader> currentHeader = null;
     private ByteArrayOutputStream bodyStream = null;
     private WritableByteChannel bodyChannel = null;
 
     private static final int INITIAL_BUFFER_SIZE = 4096;
 
+    /**
+     * <p>Consume a {@link StreamingPage.Chunk} and return a complete {@link Entity} if we finished one.</p>
+     *
+     * <p>NB: feeding chunks out of the expected order (e.g. bytes without a header first) is Not Allowed.
+     * Don't Do That. In particular, it might cause NPEs.</p>
+     * @param chunk a {@link StreamingPage.Chunk}
+     * @return an {@link Entity} if this chunk finished one
+     */
     public Optional<Entity<EntityHeader>> transform(StreamingPage.Chunk<EntityHeader> chunk) {
         if (chunk instanceof StreamingPage.Chunk.Header) {
             currentHeader = (StreamingPage.Chunk.Header<EntityHeader>) chunk;
-            // TODO: maybe look at content-length header to preallocate the buffer?
+            // TODO: Maybe look at content-length header to preallocate the buffer? Could do some special-casing so
+            //       we don't always have to call toHttpHeaders...
+            //       In that case, limit it to avoid the DoS case of huge content-length.
             bodyStream = new ByteArrayOutputStream(INITIAL_BUFFER_SIZE);
             bodyChannel = Channels.newChannel(bodyStream);
             return Optional.empty();
@@ -29,12 +46,12 @@ class ToCompleteEntitiesTransformer<EntityHeader extends ToHttpHeaders> {
             try {
                 bodyChannel.write(((StreamingPage.Chunk.BodyChunk<EntityHeader>) chunk).data());
             } catch (IOException e) {
-                throw new IllegalStateException("IOException when writing to in-memory buffer; how?", e);
+                throw new IllegalStateException("IOException when writing to in-memory buffer; weird; bug?", e);
             }
             return Optional.empty();
         } else if (chunk instanceof StreamingPage.Chunk.BodyEnd) {
             // TODO: Alternative implementation: keep a List<ByteBuffer> and build a Body impl from that. Doesn't
-            //  require a big new allocation, but is just generally less efficient?
+            //  require a big new allocation, but feels just generally less efficient?
             final Body body = Body.fromBytes(bodyStream.toByteArray(), currentHeader.contentType());
             final Entity<EntityHeader> entity = new Entity<>(currentHeader.header(), body);
             return Optional.of(entity);
