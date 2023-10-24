7fa983e3a54540fbfd1c0d75cf2a11a2595e4f6c
Benjamin Mette <benjamin.mette@neuland-bfi.de>
2023-10-24 13:35:55 +0200

    WIP

--- src/main/java/io/datareplication/producer/snapshot/SnapshotProducerImpl.java
+++ src/main/java/io/datareplication/producer/snapshot/SnapshotProducerImpl.java
@@ -1,17 +1,31 @@
 package io.datareplication.producer.snapshot;
 
 import io.datareplication.model.Entity;
+import io.datareplication.model.PageId;
 import io.datareplication.model.Timestamp;
+import io.datareplication.model.Url;
 import io.datareplication.model.snapshot.SnapshotEntityHeader;
 import io.datareplication.model.snapshot.SnapshotId;
 import io.datareplication.model.snapshot.SnapshotIndex;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.FlowableConverter;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.core.SingleSource;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
+import org.reactivestreams.FlowAdapters;
+import org.reactivestreams.Subscriber;
 
 import java.time.Clock;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.Flow;
+import java.util.concurrent.Future;
 
 // TODO: impl the impl
 @AllArgsConstructor
@@ -30,7 +44,26 @@ class SnapshotProducerImpl implements SnapshotProducer {
     ) {
         SnapshotId id = snapshotIdProvider.newSnapshotId();
         Timestamp createdAt = Timestamp.of(clock.instant());
-        SnapshotIndex snapshotIndex = new SnapshotIndex(id, createdAt, Collections.emptyList());
-        return snapshotIndexRepository.save(snapshotIndex).thenApply(unused -> snapshotIndex);
+
+
+        CompletionStage<CompletionStage<SnapshotIndex>> completionStage = Flowable
+            .fromPublisher(FlowAdapters.toPublisher(entities))
+            // TODO: Replace Buffer with correct perPage grouping
+            .buffer(2)
+            .map(x -> {
+                PageId pageId = pageIdProvider.newPageId();
+                return snapshotPageUrlBuilder.pageUrl(id, pageId);
+                // TODO: Save Page to repo
+            })
+            .reduce(new ArrayList<Url>(), (urls, url) -> {
+                urls.add(url);
+                return urls;
+            })
+            .map(pages -> new SnapshotIndex(id, createdAt, pages))
+            .flatMap(snapshotIndex -> snapshotIndexRepository.save(snapshotIndex).thenApply(unused -> snapshotIndex))
+            .toCompletionStage();
+
+        return completionStage;
+        //return snapshotIndexRepository.save(snapshotIndex).thenApply(unused -> snapshotIndex);
     }
 }
