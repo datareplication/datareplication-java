ccf155945db7142ed3fc65839ca0cb0bcf6e0160
Felix Krull <felix.krull@neuland-bfi.de>
2024-01-26 16:11:52 +0100

    I guess we don't need the thread pool abstraction for now

--- src/main/java/io/datareplication/threadpool/DatareplicationThreadPool.java
+++
@@ -1,7 +0,0 @@
-package io.datareplication.threadpool;
-
-public class DatareplicationThreadPool {
-    // TODO: ??? no idea how to handle this if we want to hide the internal async impl and thus exact type of thread
-    //       pool, but specifically need a way to get the unwrapped threadpool into the internal classes without
-    //       exposing it
-}
