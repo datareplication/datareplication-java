e6393a6f887a3a9f0a5ad31f411101a30225e4d1
Felix Krull <113671193+felixkrull-neuland@users.noreply.github.com>
2024-07-02 18:40:35 +0200

    docs: producers

--- src/main/java/io/datareplication/producer/feed/FeedProducerJournalRepository.java
+++ src/main/java/io/datareplication/producer/feed/FeedProducerJournalRepository.java
@@ -8,6 +8,19 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletionStage;
 
+/**
+ * Repository to store rollback information for the feed producer.
+ * <p>
+ * The feed producer uses this information to cleanly roll back partial changes in case of a crash or other
+ * interruption. The exact meaning of the stored fields is an implementation detail and might change
+ * (in a compatible way). The repository doesn't need to know what the fields mean, it just needs to save and load
+ * their values accurately.
+ * <p>
+ * This repository always stores either zero or one records, so primary keys or indices are unnecessary.
+ *
+ * <h2>Consistency Requirements</h2>
+ * When the
+ */
 public interface FeedProducerJournalRepository {
     @Value
     class JournalState {
--- src/main/java/io/datareplication/producer/snapshot/package-info.java
+++ src/main/java/io/datareplication/producer/snapshot/package-info.java
@@ -15,5 +15,19 @@
  *
  * Then call {@link io.datareplication.producer.snapshot.SnapshotProducer#produce(java.util.concurrent.Flow.Publisher)}
  * with a {@link java.util.concurrent.Flow.Publisher} of entities to create a snapshot.
+ *
+ * <h2>Repository Implementations</h2>
+ * The snapshot producer itself only needs write access to its repositories: it generates pages
+ * and an index and hands them to the repository. The repository interfaces only define write access
+ * because the producer never needs to read the pages. This allows the repository implementation a lot of flexibility
+ * because it only to consider the needs of its own HTTP server when deciding how to save pages.
+ *
+ * <h3>Consistency Requirements</h3>
+ * Repositories must make sure that when the {@link java.util.concurrent.CompletionStage} returned by a
+ * repository method succeeds, the data has been successfully saved. Conversely, if saving fails the
+ * CompletionStage should also fail.
+ * <p>
+ * The HTTP server should ensure to only make snapshots available that had their index fully saved, but this is the
+ * responsibility of the server implementation and does not impact the producer's use of these repositories.
  */
 package io.datareplication.producer.snapshot;
